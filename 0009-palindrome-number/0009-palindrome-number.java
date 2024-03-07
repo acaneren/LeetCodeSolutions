9f3e821323c4f58729d46a500df0550255cd9e9a
acaneren <152054072+acaneren@users.noreply.github.com>
2024-03-07 13:58:31 +0300

    Time: 4 ms (100.00%), Space: 43.8 MB (79.30%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num = 0;
+        int originalnum = x;
+        if(x < 0) {
+        	
+        	return false;
+        }
+        else {
+        	
+        	while(x >= 10) {
+        		
+        		num =(num * 10) + (x % 10);
+        		x = x / 10;
+        	}
+        	num = (num * 10) + x;
+        	
+        	if(num == originalnum) {
+        		
+        		return true;
+        	}
+        	else {
+        		return false;
+        	}
+        }
+    	
+    }
+}
