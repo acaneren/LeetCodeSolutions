994bfa48c741eddb8afe097733c45ce7a3e17a14
acaneren <152054072+acaneren@users.noreply.github.com>
2024-03-11 14:41:31 +0300

    Time: 4 ms (74.70%), Space: 44.7 MB (28.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        if(s.length() != t.length()) {
+        /*if(s.length() != t.length()) {
         	return false;
-        }
+        }*/
         
     	char s1[] = s.toCharArray();
     	char s2[] = t.toCharArray();
