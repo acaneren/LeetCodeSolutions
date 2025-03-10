4448f058177227af948ccb622635aa07e769592d
acaneren <152054072+acaneren@users.noreply.github.com>
2025-03-10 14:27:51 +0300

    Time: 3 ms (74.43%), Space: 43.1 MB (76.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,30 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-    	String my_str = "";
+    	int pointer_left = 0;
+    	int pointer_right = s.length()-1;
+    	s = s.toLowerCase();
     	
-    	for(int i=0; i < s.length(); i++) {
-    		
-    		if( (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') || (s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
-    			my_str = my_str + s.charAt(i);
-    		}
+    	while(pointer_left < pointer_right) {
+    	
+	    	while( (pointer_left < pointer_right) && (!Character.isLetterOrDigit(s.charAt(pointer_left))) ) {
+	    		pointer_left++;
+	    	}
+	    	while( (pointer_right > pointer_left) && (!Character.isLetterOrDigit(s.charAt(pointer_right))) ) {
+	    		pointer_right--;
+	    	}
+	    	if(s.charAt(pointer_left) != s.charAt(pointer_right)) {
+	    		return false;
+	    	}
+	    	pointer_left++;
+	    	pointer_right--;
     	}
-    	
-    	my_str = my_str.toLowerCase();
-    	
-    	String reversed_my_str = "";
-    	
-    	for (int i = (my_str.length()-1) ; i >= 0 ; i--) {
-    		
-    		reversed_my_str = reversed_my_str + my_str.charAt(i);
-    	}
-    	
-    	if (my_str.equals(reversed_my_str)) {
-    		
-    		return true;
-    	}
-    	else {
-    		return false;
-    	}
+    	return true;
     }
 }
