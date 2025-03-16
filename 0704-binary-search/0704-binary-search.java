ee201b644442d2b751c7f0574614793b734c86fb
acaneren <152054072+acaneren@users.noreply.github.com>
2025-03-16 11:23:43 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,22 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-     
-    	while(start <= end) {	
-    		int mid = start + (end - start) / 2;
-    		
-    		if(nums[mid] == target) {
-    			return mid;
-    		}
-    		else if(nums[mid] < target) {
-    			start = mid + 1;
-    		}
-    		else {
-    			end = mid - 1;
-    		}
-    	}
-    	
-    	return -1;
+        int left = 0;
+        int mid = nums.length / 2;
+        int right = nums.length - 1;
+        
+        while(left < right) {
+        	
+        	if(nums[mid] == target) {
+        		return mid;
+        	}
+        	else if(nums[mid] > target) {
+        		right = mid - 1;
+        		mid = (left + right) / 2;
+        	}
+        	else {
+        		left = mid + 1;
+        		mid = (left + right) / 2;
+        	}
+        }
+        return -1;
     }
 }
