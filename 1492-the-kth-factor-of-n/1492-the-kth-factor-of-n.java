a8994683c096df267d4446c90cca3b57ade0f63f
acaneren <152054072+acaneren@users.noreply.github.com>
2025-10-05 21:10:10 +0300

    Time: 1 ms (39.24%), Space: 40.6 MB (59.76%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,18 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int kthFactor(int n, int k) {
-        
-        int counter= 0;
-        for(int i=1; i <= n; i++){
+        List<Integer> second_half = new ArrayList<>();
+
+        int counter = 0;
+        for(int i=1; i * i <= n; i++){
             if(n % i == 0){
                 counter++;
                 if(counter == k){
                     return i;
                 }
+                if(i != n/i){
+                    second_half.add(0,n/i);
+                }
             }
         }
-        if(counter < k){
-            return -1;
+
+        int index = k - counter - 1;
+        if(index >= 0 && index < second_half.size()){
+            return second_half.get(index);
         }
-        return 0;
+        return -1;
     }
 }
