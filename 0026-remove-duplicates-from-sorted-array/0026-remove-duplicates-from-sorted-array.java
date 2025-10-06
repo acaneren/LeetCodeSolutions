22021a19e3df5b5e0e5b710eea55d1ee6eea27c9
acaneren <152054072+acaneren@users.noreply.github.com>
2025-10-06 12:00:16 +0300

    Time: 1 ms (58.43%), Space: 45.4 MB (6.73%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int current_num = nums[0];
         int left = 1;
         
-        for(int i=0; i < nums.length; i++){
+        for(int i=1; i < nums.length; i++){
             if(nums[i] != current_num){
                 current_num = nums[i];
                 
