693e55a3b1dbccb8e338f9ebabba010c0431c333
acaneren <152054072+acaneren@users.noreply.github.com>
2025-10-09 01:00:43 +0300

    Time: 1 ms (99.67%), Space: 46.7 MB (7.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int left = 0;
-        int counter = 0;
 
         for(int i=0; i < nums.length; i++){
             if(nums[i] == 0) {
-                counter++;
                 continue;
             }
             nums[left] = nums[i];
             left++;
         }
-        
-        int right = nums.length-1;
-        while(counter != 0){
-            nums[right] = 0;
-            right--;
-            counter--;
+
+        while(left < nums.length){
+            nums[left] = 0;
+            left++;
         }
     }
 }
