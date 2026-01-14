c2f5e2a3cb5be4e5d7c1190b9cb263e58531a4c9
acaneren <152054072+acaneren@users.noreply.github.com>
2026-01-14 23:58:26 +0300

    Time: 4 ms (9.22%), Space: 44.5 MB (16.44%) - LeetHub

---
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int residuePrefixes(String s) {
+        Set<Character> set = new HashSet<>();
+        int counter = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+            if (set.size() == (i + 1) % 3) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
